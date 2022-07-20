f21fd3a0989b551a705730906ce580564d5ce67b
grace-matson <gracematson@google.com>
2022-07-20 14:40:38 +0000

    edit workflow

--- .github/scripts/premerge-validate.py
+++ .github/scripts/premerge-validate.py
@@ -14,8 +14,8 @@ bucket_name = 'hub-cdap-io'
 bucket_dir = 'v2/'
 only_warning_types =['create_driver_artifact']
 
-##1. FETCHING ADDED/MODIFIED PLUGINS
-#Getting list of added plugins and modified plugins, and concatenating them
+##1. FETCHING ADDED/MODIFIED ARTIFACTS
+#Getting list of added artifacts and modified artifacts, and concatenating them
 added_list = ast.literal_eval(os.getenv('ADDED_LIST'))
 modified_list = ast.literal_eval(os.getenv('MODIFIED_LIST'))
 renamed_list = ast.literal_eval(os.getenv('RENAMED_LIST'))
@@ -24,78 +24,78 @@ logging.debug('List of added or modified files within pull request')
 logging.debug(am_list)
 
 
-specfiles = [] #storing the modified spec.json file names
-modifiedPlugins = [] #storing the modified plugins as <plugin>/<version> format
+specFiles = [] #storing the modified spec.json file names
+modifiedArtifacts = [] #storing the modified artifacts as <artifact>/<version> format
 
 #loop to check for modified spec.json files
 for file in am_list:
   if(file.split('/')[-1]=="spec.json"):
     # example of file name:
     # packages/database-plugin-db2-plugin/1.2.0/spec.json
-    specfiles.append(file)
-    plugin = file.split('/')[1]
+    specFiles.append(file)
+    artifact = file.split('/')[1]
     version = file.split('/')[2]
-    modifiedPlugins.append(plugin+'/'+version)
+    modifiedArtifacts.append(artifact + '/' + version)
 
-#logging the final list of plugin version which were added/modified
-logging.debug("Modified plugins are (where spec.json was modified/added) :")
-logging.debug(modifiedPlugins)
+#logging the final list of artifact versions which were added/modified
+logging.debug("Modified artifacts are (where spec.json was modified/added) :")
+logging.debug(modifiedArtifacts)
 logging.debug("Spec.json files are :")
-logging.debug(specfiles)
+logging.debug(specFiles)
 
-if(len(specfiles)==0):
+if(len(specFiles)==0):
   #exiting successfully if none of the modified/added files are spec.json
   sys.exit(0)
 
 ##2. CHECKING PACKAGES.JSON FILE
 packagesList = json.loads(open("./packages.json", "r").read())
 # converting list to dictionary format to access easily later
-mod_packagesDict = dict([(plugin['name'] + '/' + plugin['version'], plugin)  # Key: "<plugin_name>/<version>" Value: artifact object in packagesList
-                         for plugin in packagesList
-                         if plugin['name'] +'/' + plugin['version'] in modifiedPlugins]) # only appending those plugins which are modified/added
+mod_packagesDict = dict([(artifact['name'] + '/' + artifact['version'], artifact)  # Key: "<artifact_name>/<version>" Value: artifact object in packagesList
+                         for artifact in packagesList
+                         if artifact['name'] + '/' + artifact['version'] in modifiedArtifacts]) # only appending those artifacts which are modified/added
 logging.debug("Dictionary of modified artifacts: \n")
 logging.debug(mod_packagesDict)
 
-if(len(mod_packagesDict)!=len(modifiedPlugins)):
-  #Exit failure if the no.of modified plugins in the packages.json file is not the same as the no.of modified plugins
-  sys.exit("no.of modified plugins in the packages.json file is not the same as the no.of modified plugins")
+if(len(mod_packagesDict)!=len(modifiedArtifacts)):
+  #Exit failure if the no.of modified artifacts in the packages.json file is not the same as the no.of modified artifacts
+  sys.exit("no.of modified artifacts in the packages.json file is not the same as the no.of modified artifacts")
 
-for index, plugin in enumerate(modifiedPlugins):
-  specFile = json.loads(open(specfiles[index], "r").read())
-  logging.debug("\n\n Printing specFile for "+ plugin)
+for index, artifact in enumerate(modifiedArtifacts):
+  specFile = json.loads(open(specFiles[index], "r").read())
+  logging.debug("\n\n Printing specFile for " + artifact)
   logging.debug(json.dumps(specFile, indent=2))
 
-  packagesDictObject = mod_packagesDict[plugin]
-  logging.debug("\n\n Printing packages.json info for "+ plugin)
+  packagesDictObject = mod_packagesDict[artifact]
+  logging.debug("\n\n Printing packages.json info for " + artifact)
   logging.debug(json.dumps(packagesDictObject, indent=2))
 
   #Validating packages.json
   if('cdapVersion' in specFile and not(specFile['cdapVersion']==packagesDictObject['cdapVersion'])):
-    sys.exit("Fields do not match in packages.json and the added plugins")
+    sys.exit("Fields do not match in packages.json and the added artifacts")
 
 else :
-  logging.debug("Success, all modified/added plugin versions are added in packages.json")
+  logging.debug("Success, all modified/added artifact versions are added in packages.json")
 
 
-##3. ITERATING THROUGH THE MODIFIED PLUGINS AND CHECKING IF ALL THE REQUIRED DEPENDENCIES ARE RETRIEVABLE
+##3. ITERATING THROUGH THE MODIFIED ARTIFACTS AND CHECKING IF ALL THE REQUIRED DEPENDENCIES ARE RETRIEVABLE
 
-#iterating through each plugin
-for specfile in specfiles:
-  #example specfile = "packages/database-plugin-db2-plugin/1.3.0/spec.json"
-  pathList = specfile.split('/')
-  artifactDir = os.path.join(pathList[0], pathList[1]) #plugin directory ex: "packages/database-plugin-db2-plugin"
-  artifactVersionDir = specfile[:-10] #plugin version directory ex: "packages/database-plugin-db2-plugin/1.3.0"
+#iterating through each artifact
+for specFile in specFiles:
+  #example specFile = "packages/database-plugin-db2-plugin/1.3.0/spec.json"
+  pathList = specFile.split('/')
+  artifactDir = os.path.join(pathList[0], pathList[1]) #artifact directory ex: "packages/database-plugin-db2-plugin"
+  artifactVersionDir = os.path.join(artifactDir, pathList[2]) #artifact version directory ex: "packages/database-plugin-db2-plugin/1.3.0"
 
   logging.debug(f'Inspecting spec.json of {artifactVersionDir} for required files') #required files = jar or json files listed in actions field of spec.json file
-  specData = json.loads(open(specfile, "r").read()) #loading json data in spec.json as dictionary
+  specData = json.loads(open(specFile, "r").read()) #loading json data in spec.json as dictionary
   necessaryFiles = [] #list of files which need to be retrieved from GCS or Maven Central
   only_warn= []
   for object in specData['actions']:
-    warn = False
-    if object['type'] in only_warning_types:
-      warn = True
     for property in object['arguments']:
       if(property['name'] == 'jar' or property['name'] == 'config'): #json file names are under config property, and jar file names under jar property
+        warn = False
+        if (object['type'] in only_warning_types and property['name'] == 'config'):
+          warn = True
         requiredFile = os.path.join(artifactVersionDir, property['value'])
         #ex: packages/database-plugin-db2-plugin/1.3.0/db2-plugin-1.3.0.json
         if(not(os.path.isfile(requiredFile))):
