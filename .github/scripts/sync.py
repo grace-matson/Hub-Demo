fad9fb698196f8c4dcb8c5f8b8636ea84f4b3888
grace-matson <gracematson@google.com>
2022-07-21 12:22:39 +0000

    update sync.py

--- .github/scripts/sync.py
+++ .github/scripts/sync.py
@@ -1,3 +1,3 @@
 import utilities
 
-utilities.run_shell_command('gsutil -m rsync -r ./testdir gs://${CENTRAL_BUCKET}/')
+utilities.run_shell_command('gsutil -m rsync -d -c -r -n testdir/ gs://${CENTRAL_BUCKET}/packages')
